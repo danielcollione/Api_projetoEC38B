7f19fa34dd91d099d37baece82e823bc8f097c28
danielcollione <daniel.collione22@gmail.com>
2020-11-03 21:01:19 -0300

    uptade
    configura porta heroku

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./config/express')();
 const port = app.get('port');
 
 // RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 });
